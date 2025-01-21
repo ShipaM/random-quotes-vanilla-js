b44a093ccf35208ea7bba2e4a1723c5c341d62b3
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-01-21 19:04:09 +0200

    feat(add-modules): splitting app into the JS modules

--- script.js
+++ script.js
@@ -1,45 +1,4 @@
-const quotes = [
-  {
-    quote: "The only way to do great work is to love what you do.",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Innovation distinguishes between a leader and a follower.",
-    author: "Steve Jobs",
-  },
-  {
-    quote:
-      "Your time is limited, so don't waste it living someone else's life.",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "You miss 100% of the shots you don't take.",
-    author: "Wayne Gretzky",
-  },
-  {
-    quote: "Life is what happens when you're busy making other plans.",
-    author: "John Lennon",
-  },
-  {
-    quote:
-      "The future belongs to those who believe in the beauty of their dreams.",
-    author: "Eleanor Roosevelt",
-  },
-  {
-    quote:
-      "The only limit to our realization of tomorrow will be our doubts of today.",
-    author: "Franklin D. Roosevelt",
-  },
-  {
-    quote: "You must be the change you wish to see in the world.",
-    author: "Mahatma Gandhi",
-  },
-  {
-    quote:
-      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-    author: "Winston Churchill",
-  },
-];
+import quotes from "./quote.js";
 
 const quoteElement = document.getElementById("quote");
 const quoteِAuthorElement = document.getElementById("quote-author");
