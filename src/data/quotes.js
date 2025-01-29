0109c4b5e8dd61b587bd034a38994e32ff7a1751
Maksym Shypytsia <maksym.shypytsia@cd.p2h.com>
2025-01-29 11:21:41 +0200

    feat(add-id):  add unique id for array quotes

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,45 +1,54 @@
 const texts = [
   {
+    id: "bf96f091",
     text: "The only way to do great work is to love what you do.",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
+    id: "e9c1bba9",
     text: "Innovation distinguishes between a leader and a follower.",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
+    id: "4217dc",
     text: "Your time is limited, so don't waste it living someone else's life.",
     author: "Steve Jobs",
     isFavorite: false,
   },
   {
+    id: "88d9ca45",
     text: "You miss 100% of the shots you don't take.",
     author: "Wayne Gretzky",
     isFavorite: false,
   },
   {
+    id: "964084b8",
     text: "Life is what happens when you're busy making other plans.",
     author: "John Lennon",
     isFavorite: false,
   },
   {
+    id: "8eb3fb1f",
     text: "The future belongs to those who believe in the beauty of their dreams.",
     author: "Eleanor Roosevelt",
     isFavorite: false,
   },
   {
+    id: "a6cde724",
     text: "The only limit to our realization of tomorrow will be our doubts of today.",
     author: "Franklin D. Roosevelt",
     isFavorite: false,
   },
   {
+    id: "a67de724",
     text: "You must be the change you wish to see in the world.",
     author: "Mahatma Gandhi",
     isFavorite: false,
   },
   {
+    id: "a6cdeb24",
     text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
     author: "Winston Churchill",
     isFavorite: false,
